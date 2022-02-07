10ad4ceadab29f0c1bca0df0c1912a73ec30081c
Elías De Vargas <elias.devargas@zemoga.com>
2022-02-06 19:58:53 -0500

    adjust imports

--- angular13-workshop/src/app/Infraestructure/services/lessons/lessons.service.ts
+++ angular13-workshop/src/app/Infraestructure/services/lessons/lessons.service.ts
@@ -3,21 +3,21 @@ import { from } from 'rxjs';
 import { Lesson } from '../../models/lesson';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class LessonsService {
-  lessons: Lesson[] = [
-    { title: 'Hello Angular' },
-    { title: 'Component Fundamentals' },
-    { title: 'Template Driven Forms' },
-    { title: 'Angular Services' },
-    { title: 'Server Communication' },
-    { title: 'Component Driven Architecture' },
-    { title: 'Angular Routing' },
-    { title: 'Unit Testing Fundamentals' },
-  ];
+    lessons: Lesson[] = [
+        { title: 'Hello Angular' },
+        { title: 'Component Fundamentals' },
+        { title: 'Template Driven Forms' },
+        { title: 'Angular Services' },
+        { title: 'Server Communication' },
+        { title: 'Component Driven Architecture' },
+        { title: 'Angular Routing' },
+        { title: 'Unit Testing Fundamentals' },
+    ];
 
-  lessons$ = from(this.lessons)
+    lessons$ = from(this.lessons);
 
-  constructor() { }
+    constructor() {}
 }
