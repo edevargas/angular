9c3679bca9186ee4775360da56162c4f7e016f59
Elías De Vargas <sailedvc@gmail.com>
2022-02-19 21:20:00 -0500

    create libs and components

--- production-grade/apps/dashboard/src/app/app.module.ts
+++ production-grade/apps/dashboard/src/app/app.module.ts
@@ -5,10 +5,33 @@ import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { HttpClientModule } from '@angular/common/http';
 import { StoreModule } from '@ngrx/store';
+import { CoreDataModule, coreDataRoutes } from '@devangular/core-data';
+import { RoutingModule } from './routing.module';
+import { WidgetsListComponent } from './widgets/widgets-list/widgets-list.component';
+import { DetailsComponent } from './widgets/details/details.component';
+import { WidgetsComponent } from './widgets/widgets.component';
+import { HomeComponent } from './home/home.component';
+import { MaterialModule, materialRoutes } from '@devangular/material';
+import { CoreStateModule, coreStateRoutes } from '@devangular/core-state';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, HttpClientModule, StoreModule.forRoot({}, {})],
+  declarations: [
+    AppComponent,
+    NxWelcomeComponent,
+    WidgetsListComponent,
+    DetailsComponent,
+    WidgetsComponent,
+    HomeComponent,
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    StoreModule.forRoot({}, {}),
+    CoreDataModule,
+    RoutingModule,
+    MaterialModule,
+    CoreStateModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
