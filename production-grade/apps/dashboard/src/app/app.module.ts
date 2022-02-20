a293217cd191aa2311521dbe19e4c6c4ff167866
Elías De Vargas <sailedvc@gmail.com>
2022-02-19 21:41:04 -0500

    add material modules and organize imports

--- production-grade/apps/dashboard/src/app/app.module.ts
+++ production-grade/apps/dashboard/src/app/app.module.ts
@@ -1,36 +1,34 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { CoreDataModule } from '@devangular/core-data';
+import { CoreStateModule } from '@devangular/core-state';
+import { MaterialModule } from '@devangular/material';
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
-import { HttpClientModule } from '@angular/common/http';
-import { StoreModule } from '@ngrx/store';
-import { CoreDataModule, coreDataRoutes } from '@devangular/core-data';
-import { RoutingModule } from './routing.module';
-import { WidgetsListComponent } from './widgets/widgets-list/widgets-list.component';
-import { DetailsComponent } from './widgets/details/details.component';
-import { WidgetsComponent } from './widgets/widgets.component';
 import { HomeComponent } from './home/home.component';
-import { MaterialModule, materialRoutes } from '@devangular/material';
-import { CoreStateModule, coreStateRoutes } from '@devangular/core-state';
+import { NxWelcomeComponent } from './nx-welcome.component';
+import { RoutingModule } from './routing.module';
+import { DetailsComponent } from './widgets/details/details.component';
+import { WidgetsListComponent } from './widgets/widgets-list/widgets-list.component';
+import { WidgetsComponent } from './widgets/widgets.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NxWelcomeComponent,
-    WidgetsListComponent,
     DetailsComponent,
-    WidgetsComponent,
     HomeComponent,
+    NxWelcomeComponent,
+    WidgetsComponent,
+    WidgetsListComponent,
   ],
   imports: [
     BrowserModule,
-    HttpClientModule,
-    StoreModule.forRoot({}, {}),
     CoreDataModule,
-    RoutingModule,
-    MaterialModule,
     CoreStateModule,
+    HttpClientModule,
+    MaterialModule,
+    RoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent],
